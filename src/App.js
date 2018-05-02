583aeb7824bbccbf661b88817b960019baef7f24
Benedikt Werner <1benediktwerner@gmail.com>
2018-05-02 13:45:57 +0200

    Fix typo in index page title

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Formula Manger</h1>
+          <h1 className="App-title">Formula Manager</h1>
         </header>
 
         <div className="container">
